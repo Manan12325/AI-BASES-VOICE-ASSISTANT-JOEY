a81885e1ee8c95badea827d6dff83c7180d68c50
Manan12325 <22BCS12636@cuchd.in>
2025-05-27 10:29:59 +0530

    Add files via upload

---
+++ AI BASES VOICE ASSISTANT JOEY.py
@@ -0,0 +1,48 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:\Users\manan\.ipython\vps.py ===================
+Loading your AI personal assistant - Joey
+Good Morning
+Joey
+
+
+==============
+   LISTENING  
+==============
+
+Recognizing...
+User said:,good morning
+
+==============
+   LISTENING  
+==============
+
+Recognizing...
+User said:,what's the time
+
+==============
+   LISTENING  
+==============
+
+Recognizing...
+User said:,switch off send email
+
+==============
+   LISTENING  
+==============
+
+Recognizing...
+User said:,Assalam Walekum
+
+==============
+   LISTENING  
+==============
+
+Recognizing...
+User said:,voice assistant
+
+==============
+   LISTENING  
+==============
+
